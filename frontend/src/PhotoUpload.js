bee49cdc8fc9ffe91f9ce0811559e813f3d0bd7a
wfsfdg <rikychoi@gmail.com>
2024-06-11 01:32:53 +0900

    6/11_01:32

--- frontend/src/PhotoUpload.js
+++ frontend/src/PhotoUpload.js
@@ -1,43 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './css/PhotoUpload.css';
 
 const PhotoUpload = () => {
-  return (
-    <div className="container-center-horizontal">
-      <div className="PhotoUpload screen">
-        <div className="sign-in-1 sign-in-3">
-          <h1 className="h1-enter-title">h1태그 Enter title</h1>
-          <p className="text-3 lato-normal-mine-shaft-25px">
-            본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문
-            본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문
-            본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문
-          </p>
+    const [tag, setTag] = useState('');
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+
+    return (
+        <div className="container-center-horizontal">
+            <div className="PhotoUpload screen">
+                <div className="sign-in-1 sign-in-3">
+                    <input
+                        type="text"
+                        className="h1-enter-title"
+                        placeholder="h1태그 Enter title"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
+                </div>
+                <div className="sign-in-container sign-in-3 lato-normal-mine-shaft-25px">
+                    <div className="sign-in">
+                        <input
+                            type="text"
+                            className="enter-tag-tag"
+                            placeholder="Enter tag (#tag)"
+                            value={tag}
+                            onChange={(e) => setTag(e.target.value)}
+                        />
+                    </div>
+                    <div className="sign-in">
+                        <div className="select-attachment">Select attachment</div>
+                    </div>
+                </div>
+                <div className="group-12">
+                    <div className="sign-in-2 sign-in-3">
+                        <textarea
+                            className="enter-text lato-normal-mine-shaft-25px"
+                            placeholder="Enter text"
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                            rows="25"  // 텍스트 영역의 높이를 조정합니다.
+                        />
+                    </div>
+                </div>
+                <div className="button">
+                    <div className="sign-up valign-text-middle">Post</div>
+                </div>
+            </div>
         </div>
-        <div className="sign-in-container sign-in-3 lato-normal-mine-shaft-25px">
-          <div className="sign-in">
-            <div className="enter-tag-tag">Enter tag (#tag)</div>
-            <p className="text">
-              본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문
-              본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문
-              본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문
-            </p>
-          </div>
-          <div className="sign-in">
-            <div className="select-attachment">Select attachment</div>
-            <p className="text">
-              본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문
-              본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문
-              본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문 본문
-            </p>
-          </div>
-        </div>
-        <div className="group-12">
-          <div className="sign-in-2 sign-in-3"><div className="enter-text lato-normal-mine-shaft-25px">Enter text</div></div>
-        </div>
-        <div className="button"><div className="sign-up valign-text-middle">Post</div></div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default PhotoUpload;
